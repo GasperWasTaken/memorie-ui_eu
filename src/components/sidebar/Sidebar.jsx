d5a2e4fffcd9746bc3553ea5f7109b1d28ef091c
Z3rodayz <gasper.galic@protonmail.com>
2022-12-05 20:59:18 +0100

    implement sidebar

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,39 @@
+import "./sidebar.css"
+import { RssFeed, Image, PersonalVideo } from "@material-ui/icons"
+
+export default function Sidebar() {
+    return (
+        <div className="sidebar">
+            <div className="sidebarWrapper">
+                <ul className="sidebarList">
+                    <li className="sidebarListItem">
+                        <RssFeed className="sidebarIcon" />
+                        <span className="sidebarListItemText">
+                            Feed
+                        </span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <Image className="sidebarIcon" />
+                        <span className="sidebarListItemText">
+                            Images
+                        </span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <PersonalVideo className="sidebarIcon" />
+                        <span className="sidebarListItemText">
+                            Videos
+                        </span>
+                    </li>
+                </ul>
+                <button className="sidebarButton">Show More</button>
+                <hr className="sidebarHr"/>
+                <ul className="sidebarFriendList">
+                    <li className="sidebarFriend">
+                        <img className="sidebarFriendImg" src="/assets/person/profilepic1.jpg" alt=""></img>
+                        <span className="sidebarFriendName">John Doe</span>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
+}
